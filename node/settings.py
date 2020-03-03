604fb83d0fe1dcb6a95bd1b70edc85bef6169127
Austin Cheng <auhcheng@gmail.com>
2020-03-03 13:44:53 -0500

    Update static

--- node/settings.py
+++ node/settings.py
@@ -169,6 +169,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Activate Django-Heroku.
 if 'HEROKU' in os.environ:
     import django_heroku
