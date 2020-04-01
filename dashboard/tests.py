62c32d2d1372bfae59fa38ac9fb7d673529a8c96
Joshua Santos <jrs5bx@virginia.edu>
2020-03-31 23:01:32 -0400

    Updated tests.py
    attempted to write some tests for the todo form

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from .views import get_weather_context
 
+from .forms import TodoForm
+from .models import Todo
+
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+
 class DashboardTests(TestCase):
 
     def test_weather_is_consistent(self):
@@ -8,3 +15,21 @@ class DashboardTests(TestCase):
         context2 = get_weather_context()
 
         self.assertEqual(context1, context2)
+
+    def test_valid_form(self):
+        form = TodoForm({'task': "task"})
+        self.assertTrue(form.is_valid())
+        task = form.save()
+        self.assertEqual(task.task, "task")
+        task.user = "user"
+        task = form.save()
+        self.assertEqual(task.user, "user")
+
+    def test_blank_form(self):
+        form = TodoForm({})
+        self.assertFalse(form.is_valid())
+
+    def test_add_task(self):
+        client = Client()
+        client.login(username='mst3k@virginia.edu', password='password')
+        res
