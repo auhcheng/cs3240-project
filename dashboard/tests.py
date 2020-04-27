8e28d06242c4bf7b5efc2b19932b5fba198491b9
Austin Cheng <auhcheng@gmail.com>
2020-04-27 12:55:42 -0400

    Update tests.py

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -13,20 +13,26 @@ from django.test.client import Client
 class DashboardTests(TestCase):
 
     def test_weather_is_consistent(self):
-        context1 = get_weather_context()
-        context2 = get_weather_context()
+        context1 = get_weather_context('')
+        context2 = get_weather_context('')
         self.assertEqual(context1, context2)
     
+    def test_weather_zip_code_is_consistent(self):
+        context1 = get_weather_context('22181')
+        context2 = get_weather_context('22181')
+        self.assertEqual(context1, context2)
+        self.assertEqual(context1['city_weather']['city'], 'Vienna')
+    
     def test_weather_context_is_dict(self):
-        context = get_weather_context()
+        context = get_weather_context('')
         self.assertIsInstance(context, dict)
     
     def test_weather_temperature_is_number(self):
-        city_weather = get_weather_context()['city_weather']
+        city_weather = get_weather_context('')['city_weather']
         self.assertIsInstance(city_weather['temperature'], float)
     
     def test_weather_description_is_string(self):
-        city_weather = get_weather_context()['city_weather']
+        city_weather = get_weather_context('')['city_weather']
         self.assertIsInstance(city_weather['description'], str)
 
     # Below tests are for the todo model and task list functionality
